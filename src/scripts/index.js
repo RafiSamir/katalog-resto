d18585a265c88222a62b1b5a8ea54f3727b570c5
RafiSamir <rafisamir.91@gmail.com>
2023-12-01 19:02:24 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../scripts/drawer';
+import '../scripts/main'
+
+console.log('Hello Coders! :)');
